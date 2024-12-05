2bca60a7715c412854202be50227099ebde87551
Kim Lida <88290393+Kinukeo@users.noreply.github.com>
2024-12-05 18:45:08 +0000

    randomise computer output and loop game

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-
 // User input stored as a perimeter of a variable✅
-
 
 // Variable with static value for computer input ✅
 
-//MVP 1
-// If statement userinput + computerinput ===
-// R > S return player 1 wins
-// S > P return player 1 wins
-// P > R return player 1 wins
-// R < P return player 2 wins
-// S < R return player 2 wins
-// P < S return player 2 wins
-// When ===  return draw
-
+/* MVP 1
+If statement userinput + computerinput ===
+R > S 
+ player 1 wins
+S > P 
+ player 1 wins
+P > R 
+ player 1 wins
+R < P 
+ player 2 wins
+S < R 
+ player 2 wins
+P < S 
+ player 2 wins
+When ===  
+ draw
+*/
 //MVP 2
 
 //Make an array with possible computer choices (rock, paper, scissors)✅
@@ -23,66 +28,72 @@
 //Then use the computer input in the function✅
 //Game loop
 
+// Encompass function in a while loop ✅
+// While playAgain = true ✅
+// Prompt user, play again? ✅
+// If yes, continue looping the function ✅
+// If no playAgain = false ✅
+// Break loop ✅
 
-// defines the three values for the game 
+// define the three values for the game
 let rock = "Rock";
 let paper = "Paper";
 let scissors = "Scissors";
 
-//array
+// array
 let gameChoice = ["Rock", "Paper", "Scissors"];
+let playAgain = false;
 
+// ask if the user wants to play
+let gameStart = prompt("Would you like to play? Please type Yes or No");
+if (gameStart === "Yes") {
+  playAgain = true;
+}
 
-// user input and game logic for rock, paper, scissors
-function userSelection(input) {
+// Declare function
+function userSelection() {
+  input = prompt("Please choose Rock, Paper or Scissors");
+  let computerInput = gameChoice[Math.floor(Math.random() * 3)];
 
+  let score = input + computerInput;
 
-    let computerInput = gameChoice[Math.floor(Math.random() * 3)];
-    let score = input + computerInput;
+  if (score === "RockRock") {
+    console.log("Draw");
+    //  "Draw";
+  } else if (score === "RockPaper") {
+    console.log("Player 2 wins");
+    //  "Player 2 wins";
+  } else if (score === "RockScissors") {
+    console.log("Player 1 wins");
+    //  "Player 1 wins";
+  } else if (score === "PaperPaper") {
+    console.log("Draw");
+    //  "Draw";
+  } else if (score === "PaperScissors") {
+    console.log("Player 2 wins");
+    //  "Player 2 wins";
+  } else if (score === "PaperRock") {
+    console.log("Player 1 wins");
+    //  "Player 1 wins";
+  } else if (score === "ScissorsScissors") {
+    console.log("Draw");
+    //  "Draw";
+  } else if (score === "ScissorsRock") {
+    console.log("Player 2 wins");
+    //  "Player 2 wins";
+  } else if (score === "ScissorsPaper") {
+    console.log("Player 1 wins");
+    //  "Player 1 wins";
+  }
+}
 
-    
-
-    if (score === "RockRock") {
-        return "Draw"
-    } else if (score === "RockPaper") {
-        return "Player 2 wins"
-    } else if (score === "RockScissors") {
-        return "Player 1 wins"
-    } else if (score === "PaperPaper") {
-        return "Draw"
-    } else if (score === "PaperScissors") {
-        return "Player 2 wins"
-    } else if (score === "PaperRock") {
-        return "Player 1 wins"
-    } else if (score === "ScissorsScissors") {
-        return "Draw"
-    } else if (score === "ScissorsRock") {
-        return "Player 2 wins"
-    } else if (score === "ScissorsPaper") {
-        return "Player 1 wins"
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// wrap function call in while loop
+while (playAgain === true) {
+  userSelection();
+  let choice = prompt(
+    "Would you like to play again? Please enter Yes or No"
+  ).toLowerCase();
+  if (choice === "no") {
+    playAgain = false;
+  }
+}
